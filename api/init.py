15cf8872383cdbc3611903c9873fa091aeee64d5
snarang181 <70980689+snarang181@users.noreply.github.com>
2022-08-26 22:04:25 -0500

    added auth key

--- api/init.py
+++ api/init.py
@@ -1,9 +1,17 @@
 from flask import Flask, request, render_template
 from flask_cors import CORS
 from flask_mail import Mail, Message
+import os, requests, json
 
 app = Flask(__name__)
+api_key = os.getenv('API_KEY')
 
-@app.route('/')
+@app.route('/test')
 def home():
-    return "Hello World!"
+    data = request.get_json()
+    try:
+        if(data['auth_key'] != api_key):
+            return {"message": "Invalid API Key",}, 401
+    except:
+        return {"message": "API key required",}, 401
+    return {"message": "Hello World!"}
