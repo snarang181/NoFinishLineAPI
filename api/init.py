d8749ea4f386c500459f1d0418dacaea21e0d5db
snarang181 <70980689+snarang181@users.noreply.github.com>
2022-08-26 21:41:51 -0500

    init test

--- api/init.py
+++ api/init.py
@@ -1,3 +1,9 @@
 from flask import Flask, request, render_template
 from flask_cors import CORS
 from flask_mail import Mail, Message
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello World!"
