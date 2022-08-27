1a7db14129591294ac48a4b0bc9d6432b38c8dab
snarang181 <70980689+snarang181@users.noreply.github.com>
2022-08-26 23:27:34 -0500

    API setup, AWS pending now.

--- api/init.py
+++ api/init.py
@@ -2,19 +2,50 @@ from flask import Flask, request, render_template
 from flask_cors import CORS
 from flask_mail import Mail, Message
 import os, requests, json
+from dotenv import load_dotenv
+from api.user_db_management import user_register
+
+load_dotenv()
 
 app = Flask(__name__)
-api_key = 'aac48d6f-b63d-47a8-ada0-731db9955679'
+api_key = os.environ.get('API_KEY')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
 
-@app.route('/test', methods=['POST'])
-def home():
+
+# @app.route('/test', methods=['POST'])
+# def home():
+#     data = request.get_json()
+#     print(data)
+#     try:
+#         if(data['auth_key'] != api_key):
+#             return {"message": "Invalid API Key",}, 401
+#     except:
+#         return {"message": "API key required",}, 401
+#     return {"message": "Hello World!"}
+
+@app.route('/', methods=['GET'])
+def landing():
+    return {'message': 'Hello, World! This is a private API.'}, 200
+
+@app.route('/user/signup', methods=['POST'])
+def signup():
     data = request.get_json()
-    print(data)
     try:
         if(data['auth_key'] != api_key):
             return {"message": "Invalid API Key",}, 401
     except:
         return {"message": "API key required",}, 401
-    return {"message": "Hello World!"}
+    
+    id  = data['id']
+    password = data['password']
+    status_code, message, auth_token, userid  = user_register(id, password)
+    return {"message": data, "auth_token": auth_token, "userID":str(userid)}, status_code
+    
+    # return user_register(data['email'], data['password'])
+
+
+
+
 
 
