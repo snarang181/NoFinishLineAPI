98d1553b25fcc1f72a9d3efa6b7f9529104de27c
snarang181 <70980689+snarang181@users.noreply.github.com>
2022-09-15 18:42:41 -0400

    Functionality for user's workout stats in progress

--- api/workout_stats.py
+++ api/workout_stats.py
@@ -3,12 +3,13 @@ from re import L
 from urllib import response
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 from passlib.hash import pbkdf2_sha256
 import random
 from dotenv import load_dotenv
 import os, requests, json, string
+
 
 load_dotenv()
 
@@ -16,6 +17,12 @@ def time():
     timeZ_Ny = pytz.timezone('America/New_York')
     now = datetime.now(timeZ_Ny)
     return now.strftime("%d/%m/%Y %H:%M:%S EST")
+
+def get_results_within(list, days):
+    today = time()
+    time_diff = datetime.strptime(today, "%d/%m/%Y %H:%M:%S EST") - timedelta(days=days)
+    filtered = [i for i in list if datetime.strptime(i['workout_date'], "%d/%m/%Y %H:%M:%S EST") > time_diff]
+    return filtered
 
 #Total Calories burned in seven days and avg over 30 days 
 def calorie_stats(user_id): 
@@ -25,9 +32,11 @@ def calorie_stats(user_id):
     try: 
         response = table.scan(FilterExpression = Attr('userid').eq(user_id))
         response = sorted(response['Items'], key = lambda i: i['workout_date'], reverse=True)
-        print(response)
+        within_week = get_results_within(response, 7)
+        within_month = get_results_within(response, 30)
+        
         return 200, response
     except Exception as e:
         return 501, str(e)
 
-calorie_stats('20852362')
+print(calorie_stats('20852362'))
