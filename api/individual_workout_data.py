647da96eff10bc36e07d04ef9a55cfd0172c40eb
snarang181 <70980689+snarang181@users.noreply.github.com>
2022-09-19 23:18:57 -0400

    API changes, formatting

--- api/individual_workout_data.py
+++ api/individual_workout_data.py
@@ -16,12 +16,12 @@ def get_workout_data(workout_name):
                 muscle_groups += m.title() + ', '
             for s in workout['secondaryMuscles']:
                 muscle_groups += s.title() + ', '
-            data['Muscle Groups'] = muscle_groups
+            data['Muscle Groups'] = muscle_groups.trim()
             data['category'] = workout['category'].title()
             instructions = ''
             for i in workout['instructions']:
                 instructions += i + ' '
-            data['instructions'] = instructions
+            data['instructions'] = instructions.trim()
             return 200, data
     return 404, "Workout not found"
 
